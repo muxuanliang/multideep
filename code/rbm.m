b53fd94a62502d7e945d08f4d25eebbed805f317
muxuanliang <mliang@stat.wisc.edu>
2015-11-05 13:15:02 -0600

    Update rbm.m

--- code/rbm.m
+++ code/rbm.m
@@ -1,10 +1,10 @@
-function [W,a,b,hs,h,d,d1] = rbm(data1,nh,nCD,nstep,nbat,maxepoch,lamda1,lamda2,scale)
+function [W,a,b,hs,h,d,d1] = rbm(data1,nh,nCD,nstep,nbat,maxepoch,lamda1,lamda2)
 %RBM is the training function for gaussian visible layer and binary hidden
 %layer rbm
 %each column represents data of single person
 [k1,k2]=size(data1);
-st = sum(var(data1));
-Win=0*randn(nh,k1);
+st = sum(var(data1'));
+Win=0.1*randn(nh,k1);
 W=Win;
 ain=zeros(k1,1);
 a=ain;
@@ -15,14 +15,15 @@ d1=zeros(maxepoch,1);
 d1(1)=norm(W);
 erro = st;
 i=1;
-while (i<= maxepoch) && (erro>scale*st)
-fprintf(1, 'epoch %4i ', i); 
+while (i<= maxepoch) && (erro> 10^-3*st)
+fprintf(1, 'epoch %4i error %12.8f  \n', i, erro); 
 [W1,a,b,erro]=TDBN1(data1,nh,W,a,b,nCD,nstep,nbat,lamda1,lamda2);
 d(i)=norm(W1-W);
 d1(i)=norm(W1);
 i=i+1;
 W=W1;
 end
+fprintf(1, 'epoch %4i error %12.8f  \n', i, erro);
 h=1./(1+exp(-W*data1-repmat(b,1,k2)));
 hs=(h>rand(size(h)));
 end
