8f042e276622f40dc722fdfc9f228085d1d249cb
muxuanliang <mliang@stat.wisc.edu>
2015-11-05 12:09:51 -0600

    Update rbm.m

--- code/rbm.m
+++ code/rbm.m
@@ -3,6 +3,7 @@ function [W,a,b,hs,h,d,d1] = rbm(data1,nh,nCD,nstep,nbat,maxepoch,lamda1,lamda2)
 %layer rbm
 %each column represents data of single person
 [k1,k2]=size(data1);
+st = sum(var(data1));
 Win=0*randn(nh,k1);
 W=Win;
 ain=zeros(k1,1);
@@ -12,10 +13,14 @@ b=bin;
 d=zeros(maxepoch,1);
 d1=zeros(maxepoch,1);
 d1(1)=norm(W);
-for i=1:maxepoch
-[W1,a,b]=TDBN1(data1,nh,W,a,b,nCD,nstep,nbat,lamda1,lamda2);
+erro = st;
+i=1;
+while (i<= maxepoch) && (erro>0.001*st)
+fprintf(1, 'epoch %4i ', i); 
+[W1,a,b,erro]=TDBN1(data1,nh,W,a,b,nCD,nstep,nbat,lamda1,lamda2);
 d(i)=norm(W1-W);
 d1(i)=norm(W1);
+i=i+1;
 W=W1;
 end
 h=1./(1+exp(-W*data1-repmat(b,1,k2)));
